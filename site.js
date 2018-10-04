716496f91764d83e384a9c6be712e5241e44f403
David Jensen <Tsourdox@icloud.com>
2018-10-04 11:05:30 +0200

    Add butto which toggles bgColor

--- site.js
+++ site.js
@@ -1,2 +1,58 @@
 
-console.log("It works!");
+
+function changeBackground() {
+    if (document.body.style.backgroundColor == "white") {
+        document.body.style.backgroundColor = "purple";
+    } else {
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pass by value
+// string, number, boolean
+
+// Pass by reference
+// array, object
+
+// var fotballPlayer = {
+//     name: "David Jensen",
+//     age: 22,
+//     shirtNumber: 14,
+//     position: "Mid-fielder",
+//     gender: "male",
+// };
+
+// var newFotballPlayer = fotballPlayer;
+// newFotballPlayer.name = "Albert Eistein";
+// console.log(newFotballPlayer);
+// console.log(fotballPlayer);
+
+
+// var message = "Welcome";
+// var newMessage = message;
+// newMessage = "Welcome home!";
+// console.log(message);
+// console.log(newMessage);
